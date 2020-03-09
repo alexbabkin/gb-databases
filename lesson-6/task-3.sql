eaf03d6cd155649a194d2e5fbf9509c33913b53a
Alexander Babkin <babkin.a@tecomgroup.ru>
2020-03-09 04:57:31 +0300

    hw for lennos 6

--- lesson-6/task-3.sql
+++ lesson-6/task-3.sql
@@ -1,1 +1,9 @@
-USE vk;SELECT (   (SELECT COUNT(*) FROM likes       WHERE user_id IN (SELECT user_id FROM profiles WHERE gender = 'f')) -   (SELECT COUNT(*) FROM likes       WHERE user_id IN (SELECT user_id FROM profiles WHERE gender = 'm')) )
+use vk
+
+SELECT 
+(
+   (SELECT COUNT(*) FROM likes
+       WHERE user_id IN (SELECT user_id FROM profiles WHERE gender = 'f')) -
+   (SELECT COUNT(*) FROM likes
+       WHERE user_id IN (SELECT user_id FROM profiles WHERE gender = 'm')) 
+)
